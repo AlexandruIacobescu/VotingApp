670a6ac68729f1507fbe47a49f09036d4112bba6
AlexandruIacobescu <Semovente2001>
2022-04-15 00:13:37 +0300

    login+voting_form

--- src/main/java/com/example/votingapp/HelloController.java
+++ src/main/java/com/example/votingapp/HelloController.java
@@ -1,12 +1,21 @@
 package com.example.votingapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
@@ -15,9 +24,10 @@ public class HelloController {
     public Button loginButton, registerButton;
     @FXML
     public TextField unameField, passField;
-
     @FXML
-    public void button1_Click() throws SQLException{
+    public TableView<Query> table;
+
+    public void button1_Click(ActionEvent e) throws SQLException, IOException {
         Connection con = DriverManager.getConnection(HelloApplication.url, HelloApplication.uname, HelloApplication.password);
         PreparedStatement stmt = con.prepareStatement("SELECT COUNT(username) FROM Accounts WHERE Username = ? AND Password = ?;");
         stmt.setString(1, unameField.getText());
@@ -25,7 +35,13 @@ public class HelloController {
         ResultSet set = stmt.executeQuery();
         set.next();
         if(set.getString(1).equals("1")){
-
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("VotingForm.fxml")));
+            Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setTitle("Voting Form");
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.show();
         }
         else{
             errorLabel.setText("Login credentials not valid");
---
+++ src/main/java/com/example/votingapp/Query.java
@@ -0,0 +1,19 @@
+package com.example.votingapp;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Query {
+    private SimpleIntegerProperty queryid;
+    private SimpleStringProperty text;
+    Query(Integer id, String text){
+        queryid = new SimpleIntegerProperty(id);
+        this.text = new SimpleStringProperty(text);
+    }
+    public String getText(){
+        return text.get();
+    }
+    public int getQueryId(){
+        return queryid.get();
+    }
+}
